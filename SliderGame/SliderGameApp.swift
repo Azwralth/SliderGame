9a7555752ecd49250425fc814fcb65152802ee55
Vladislav <mackva34@gmail.com>
2024-05-01 12:02:06 +0200

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Владислав Соколов on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
